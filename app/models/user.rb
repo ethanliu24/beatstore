71988a1e2fc87166037d0cb359ad3646cbe2ff90
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-17 12:09:21 -0400

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+
+  enum :role, { customer: 0, admin: 1 }
+end
