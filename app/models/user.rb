6fbea8001a4229e8a02840b6bedaa17e8740670d
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-17 18:15:27 -0400

    style fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,32 +3,38 @@ class User < ApplicationRecord
   enum :role, { customer: 0, admin: 1 }
 
   PASSWORD_FORMAT = /\A
-    (?=.{8,})          # Must contain 8 or more characters
-    (?=.*\d)           # Must contain a digit
-    (?=.*[a-z])        # Must contain a lower case character
-    (?=.*[A-Z])        # Must contain an upper case character
-    (?=.*[[:^alnum:]]) # Must contain a symbol
-  /x
+    (?=.{8,})           # At least 8 characters
+    (?=.*\d)            # Contains a digit
+    (?=.*[a-z])         # Contains a lowercase letter
+    (?=.*[A-Z])         # Contains an uppercase letter
+    (?=.*[[:^alnum:]])  # Contains a symbol
+  \z/x
 
   # === Validations ===
   validates :password,
     presence: true,
     length: { in: Devise.password_length },
-    format: { with: PASSWORD_FORMAT },
+    format: {
+      with: PASSWORD_FORMAT,
+      message: "Must include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character"
+    },
     confirmation: true,
     on: :create
 
   validates :password,
     allow_nil: true,
     length: { in: Devise.password_length },
-    format: { with: PASSWORD_FORMAT },
+    format: {
+      with: PASSWORD_FORMAT,
+      message: "Must include at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character"
+    },
     confirmation: true,
     on: :update
 
   validates :email,
     presence: true,
     uniqueness: { case_sensitive: false },
-    format: { with: URI::MailTo::EMAIL_REGEXP }
+    format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email format." }
 
   validates :username,
     presence: true,
