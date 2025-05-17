631bdb558ce52c964e585cc7a4c0acfa51f19b4f
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-17 12:12:12 -0400

    index confirmation token

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_153303) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
