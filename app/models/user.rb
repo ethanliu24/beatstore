c19a2006176ac3adad3a0bd6d89a293af59afaba
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-17 17:33:08 -0400

    validate user data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,40 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
-
   enum :role, { customer: 0, admin: 1 }
+
+  PASSWORD_FORMAT = /\A
+    (?=.{8,})          # Must contain 8 or more characters
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+
+  # === Validations ===
+  validates :password,
+    presence: true,
+    length: { in: Devise.password_length },
+    format: { with: PASSWORD_FORMAT },
+    confirmation: true,
+    on: :create
+
+  validates :password,
+    allow_nil: true,
+    length: { in: Devise.password_length },
+    format: { with: PASSWORD_FORMAT },
+    confirmation: true,
+    on: :update
+
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :username,
+    presence: true,
+    uniqueness: { case_sensitive: false }
+
+  validates :role,
+    presence: true,
+    inclusion: { in: roles.keys }
 end
