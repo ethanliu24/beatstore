56ed6fd50002ea22949015aa8bd8d469cbad4174
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-17 19:08:47 -0400

    use simplecov in rspec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,18 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.output_directory = 'coverage'
+  config.lcov_file_name = 'lcov.info'
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start 'rails' # or your custom profile
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
