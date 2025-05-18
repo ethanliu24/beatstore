ebb0f6c8fa15bb2b633f6b18669df0621f3220e7
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-18 10:53:34 -0400

    add home page for testing

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "pages#home"
 
   devise_for :users, controllers: { registrations: "users/registrations" }
 end
