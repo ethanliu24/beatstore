a5ae58814f05662e17a7cbf6bd206edb7bf1c18b
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-20 12:24:44 -0400

    add schema migration

---
+++ db/migrate/20250520161241_create_tracks.rb
@@ -0,0 +1,22 @@
+class CreateTracks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :tracks do |t|
+      t.string :title, null: false
+      t.string :key
+      t.string :bpm
+      t.integer :hearts, default: 0, null: false
+      t.integer :plays, default: 0, null: false
+      t.boolean :is_public, default: true, null: false
+
+      # link to track related files
+      t.string :tagged_mp3
+      t.string :untagged_mp3
+      t.string :untagged_wav
+      t.string :track_stems
+      t.string :project
+      t.string :cover_photo
+
+      t.timestamps
+    end
+  end
+end
