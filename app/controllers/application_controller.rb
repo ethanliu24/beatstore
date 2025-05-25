88a9a1c52bdef5334414eb716f6f9408be472957
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-25 18:40:00 -0400

    add tests for session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :store_previous_location, if: :storable_location?
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
+  private
+
+  def storable_location?
+    request.get? &&
+    is_navigational_format? &&
+    !devise_controller? &&
+    !request.xhr? &&
+    !user_signed_in?
+  end
+
+  def store_previous_location
+    store_location_for(:user, request.fullpath)
+  end
 end
