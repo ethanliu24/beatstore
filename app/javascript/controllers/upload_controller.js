3fb786cab273f6506f30b383cc8cb84700e71cc4
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-29 15:59:40 -0400

    file type check

--- app/javascript/controllers/upload_controller.js
+++ app/javascript/controllers/upload_controller.js
@@ -15,6 +15,11 @@ export default class extends Controller {
   }
 
   image_upload() {
+    if (!["image/png", "image/jpeg", "image/jpg"].includes(this.imgUploadTarget.files[0].type)) {
+      alert("Accepted file types: PNG, JPEG, JPG");
+      return;
+    }
+
     this.imgCoverTarget.src = URL.createObjectURL(this.imgUploadTarget.files[0]);
     this.imgCoverTarget.classList.remove("hidden");
     this.imgPlaceholderTarget.classList.add("hidden");
@@ -22,9 +27,21 @@ export default class extends Controller {
 
   track_upload(event) {
     const label = event.target.closest("label");
-    label.classList.remove("dark:bg-secondary-bg");
-    this.track_uploaded_classes.forEach((className) => {
-      label.classList.add(className);
-    })
+    const inputId = event.target.id;
+    const uploadedFile = event.target.files[0];
+    const fileType = uploadedFile ? uploadedFile.type : null;
+
+    if (
+      (inputId.includes("mp3") && fileType === "audio/mpeg") ||
+      (inputId.includes("wav") && fileType === "audio/wav") ||
+      (inputId.includes("stems") && fileType === "application/zip")
+    ) {
+      label.classList.remove("dark:bg-secondary-bg");
+      this.track_uploaded_classes.forEach((className) => {
+        label.classList.add(className);
+      });
+    } else {
+      alert("Invalid file type.");
+    }
   }
 }
