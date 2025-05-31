b6c0fe080a6fbc983af16abfb97d12e98201b8c8
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-31 17:44:57 -0400

    add metadata to form

--- app/helpers/track_upload_helper.rb
+++ app/helpers/track_upload_helper.rb
@@ -2,4 +2,28 @@ module TrackUploadHelper
   def generate_id(file_for)
     file_for.downcase.split(" ").join("-") + "-upload"
   end
+
+  def key_options
+    keys = [
+      "C", "C#",
+      "D", "D#", "Db",
+      "E", "Eb",
+      "F", "F#",
+      "G", "G#", "Gb",
+      "A", "A#", "Ab",
+      "B", "Bb"
+    ]
+    scales = [ "MAJOR", "MINOR" ]
+
+    [ nil ] + keys.product(scales).map do |key, scale|
+      "#{key} #{scale}"
+    end
+  end
+
+  def is_public_options
+    [
+      [ "Public", true ],
+      [ "Private", false ]
+    ]
+  end
 end
