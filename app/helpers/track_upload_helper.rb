5cb2304c205ecfe7c8eb8fdc9ef2af6d610750bb
ethanliu24 <liu.ethan2005@gmail.com>
2025-05-31 18:53:55 -0400

    more style fixes

--- app/helpers/track_upload_helper.rb
+++ app/helpers/track_upload_helper.rb
@@ -3,6 +3,12 @@ module TrackUploadHelper
     file_for.downcase.split(" ").join("-") + "-upload"
   end
 
+  def track_upload_style(attachment)
+    has_attachment = "bg-accent dark:bg-accent dark:text-white text-white border-none"
+    no_attachment = "border-1 dark:border-none border-secondary-txt/30 dark:bg-secondary-bg"
+    attachment.attached? ? has_attachment : no_attachment
+  end
+
   def key_options
     keys = [
       "C", "C#",
