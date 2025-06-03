14e24653115e9010744a480898e2af82ab3ad29f
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-03 10:37:10 -0400

    add more col for track

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_01_183344) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_03_141752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,6 +60,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_01_183344) do
     t.boolean "is_public", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "genre"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
