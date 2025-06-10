8c45c8d1e85297ce56ef813db53c70038d8d1b93
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-09 20:05:00 -0400

    add admin layout

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -2,6 +2,8 @@ module PageHelper
   def current_page
     if devise_controller?
       "layouts/auth_main"
+    elsif controller_path.start_with?("admin/")
+      "layouts/admin_main"
     else
       "layouts/default_main"
     end
