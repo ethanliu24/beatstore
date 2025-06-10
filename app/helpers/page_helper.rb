ef4de74402454ce01de79c55f8a5e1eb0ccf3ac0
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-09 20:42:41 -0400

    more layout change lol

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -2,7 +2,7 @@ module PageHelper
   def current_page
     if devise_controller?
       "layouts/auth_main"
-    elsif controller_path == "admin/tracks" && action_name == "new"
+    elsif controller_path == "admin/tracks" && (action_name == "new" || action_name == "edit")
       "layouts/default_main"
     elsif controller_path.start_with?("admin/")
       "layouts/admin_main"
