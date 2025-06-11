44a12a28446275921df354d4f59e493def19e1be
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-10 22:38:32 -0400

    add tests

--- spec/requests/modals_spec.rb
+++ spec/requests/modals_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Modals", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "where requests are from users" do
+    it "does not allow visits to #test" do
+      get test_modal_path
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "where request are from turbo frame" do
+    before do
+      @headers = { "Turbo-Frame" => "modal" }
+    end
+
+    it "fetches modal from #test" do
+      get test_modal_path, headers: @headers
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(partial: "modal/_test")
+    end
   end
 end
