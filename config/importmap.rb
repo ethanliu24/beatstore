ee3c2584bb33c80ad7680414a4a022dd4fac7cd7
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-12 16:53:37 -0400

    switch to cropper v1

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,16 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.turbo.min.js"
-
-pin "cropperjs" # @2.0.0
-pin "@cropper/element", to: "@cropper--element.js" # @2.0.0
-pin "@cropper/element-canvas", to: "@cropper--element-canvas.js" # @2.0.0
-pin "@cropper/element-crosshair", to: "@cropper--element-crosshair.js" # @2.0.0
-pin "@cropper/element-grid", to: "@cropper--element-grid.js" # @2.0.0
-pin "@cropper/element-handle", to: "@cropper--element-handle.js" # @2.0.0
-pin "@cropper/element-image", to: "@cropper--element-image.js" # @2.0.0
-pin "@cropper/element-selection", to: "@cropper--element-selection.js" # @2.0.0
-pin "@cropper/element-shade", to: "@cropper--element-shade.js" # @2.0.0
-pin "@cropper/element-viewer", to: "@cropper--element-viewer.js" # @2.0.0
-pin "@cropper/elements", to: "@cropper--elements.js" # @2.0.0
-pin "@cropper/utils", to: "@cropper--utils.js" # @2.0.0
+pin "cropperjs", to: "cropper.esm.js"
