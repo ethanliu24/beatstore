d0b3030b32969a60b88f220ee0f012142bf3f142
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-22 17:40:59 -0400

    extract login logic in callbakcs to own method

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,18 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    login_user(request.env["omniauth.auth"])
+  end
+
+  def failure
+    # TODO add toast
+    redirect_to new_user_session_path, alert: "Authentication failed"
+  end
+
+  private
+
+  def login_user(auth)
+    @user = User.from_omniauth(auth)
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
@@ -10,10 +21,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # TODO add toast
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
-  end
-
-  def failure
-    # TODO add toast
-    redirect_to new_user_session_path, alert: "Authentication failed"
   end
 end
