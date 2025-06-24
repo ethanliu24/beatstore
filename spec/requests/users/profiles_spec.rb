b4f7a1c35c9f06396adce3a15bc04a56b17c50f1
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-24 00:15:38 -0400

    add access tests for profiles controller

--- spec/requests/users/profiles_spec.rb
+++ spec/requests/users/profiles_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Users::ProfilesController, type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  describe "GET /edit" do
+    it "should allow authenticated users enter" do
+      sign_in user, scope: :user
+      get edit_users_profile_url
+      expect(response).to have_http_status(200)
+    end
+
+    it "should redirect unauthenticated users" do
+      get edit_users_profile_url
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(new_user_session_url)
+    end
   end
 end
