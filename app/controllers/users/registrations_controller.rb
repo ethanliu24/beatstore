448d4338974f2b2684fa438f010110f2c108e21e
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-24 01:00:42 -0400

    remove update method in registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,6 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [ :create ]
-  before_action :configure_account_update_params, only: [ :update ]
 
   protected
 
@@ -14,13 +13,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(
       :sign_up,
       keys: [ :email, :display_name, :username, :password, :password_confirmation ]
-    )
-  end
-
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(
-      :account_update,
-      keys: [ :display_name, :username, :profile_picture ]
     )
   end
 end
