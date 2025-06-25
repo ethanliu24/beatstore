d5a0e0ed560dafb2fdb48ffeed8848e933181767
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-25 00:41:04 -0400

    credentails form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,14 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [ :create ]
+  before_action :configure_account_update_params, only: [ :update ]
+
+  def update
+    super do
+      username = params[:user][:username]
+      current_user.update(username: username) if username && username != current_user.username
+    end
+  end
 
   protected
 
@@ -14,5 +22,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
       :sign_up,
       keys: [ :email, :display_name, :username, :password, :password_confirmation ]
     )
+  end
+
+  def configure_account_update_params
+    # This is for credential updates. Regular account data updates are in Users::ProfilesController
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [ :email, :username, :password, :password_confirmation, :current_password ]
+      )
   end
 end
