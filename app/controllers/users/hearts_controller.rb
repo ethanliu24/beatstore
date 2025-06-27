90eb214ead5b20932ce047b7fe485ca3b52f3436
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-27 18:41:05 -0400

    add liked tracks page

--- app/controllers/users/hearts_controller.rb
+++ app/controllers/users/hearts_controller.rb
@@ -1,5 +1,7 @@
 class Users::HeartsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @liked_tracks = current_user.hearted_tracks
+  end
 end
