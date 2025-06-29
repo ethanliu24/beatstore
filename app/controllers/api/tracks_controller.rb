e8249e20b8281a0ea469389af12efbb7754ce485
ethanliu24 <liu.ethan2005@gmail.com>
2025-06-29 17:03:07 -0400

    add api endpoint to query track data for plaeyr

---
+++ app/controllers/api/tracks_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Api::TracksController < ApplicationController
+  include ActiveStorage::SetCurrent
+
+  # Track data for audio player
+  def show
+    track = Track.find_by(id: params[:id])
+
+    if track && track.is_public?
+      render json: {
+        id: track.id,
+        title: track.title,
+        bpm: track.bpm,
+        key: track.key,
+        cover_photo: track.cover_photo.attached? ? track.cover_photo.url(expires_in: 5.minutes) : "",
+        tagged_mp3: track.tagged_mp3.attached? \
+          ? track.tagged_mp3.url(expires_in: 5.minutes)
+          : ""
+      }
+    else
+      head :not_found
+    end
+  end
+end
