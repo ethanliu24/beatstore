126e6533eda77b70bcb4c3d060356dafdf5bd440
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-01 19:23:35 -0400

    simplify play create

--- app/controllers/tracks/plays_controller.rb
+++ app/controllers/tracks/plays_controller.rb
@@ -1,7 +1,7 @@
 class Tracks::PlaysController < ApplicationController
   def create
     track = Track.find(params[:track_id])
-    Track::Play.create!(track_id: track.id, user_id: current_user ? current_user.id : nil)
+    Track::Play.create!(track_id: track.id, user_id: current_user&.id)
     head :no_content
   end
 end
