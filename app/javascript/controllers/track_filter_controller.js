f2688b26631b5a5f1183e12421d68a42520f63ab
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-16 13:02:31 -0400

    refactor update and clear checkboxes to a method

--- app/javascript/controllers/track_filter_controller.js
+++ app/javascript/controllers/track_filter_controller.js
@@ -3,8 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="track-filter"
 export default class extends Controller {
   static targets = [
-    "genreDropdown",
-    "genreChip"
+    "genreDropdown", "keyDropdown",
+    "genreChip", "keyChip",
   ];
 
   connect() {
@@ -12,40 +12,48 @@ export default class extends Controller {
   }
 
   updateGenre() {
-    const selectedGenres = Array.from(this.genreDropdownTarget.children).reduce((genres, li) => {
-      const container = li.firstElementChild;
-      const checkbox = container.firstElementChild;
-      const label = container.lastElementChild;
-      if (checkbox.checked) genres.push(label.innerText);
-      return genres;
-    }, []);
-
-    let genreStr = "";
-    if (selectedGenres.length > 3) {
-      const remainingLength = selectedGenres.length - 3;
-      genreStr = `${selectedGenres.slice(0, 3).join(", ")}, +${remainingLength}`;
-    } else if (selectedGenres.length > 0) {
-      genreStr = selectedGenres.join(", ");
-    }
-
-    const genreDisplay = this.genreChipTarget.querySelector(".track-filter-chip-text");
-    genreDisplay.innerText = genreStr;
-
-    if (genreStr === "") {
-      this.genreChipTarget.classList.add("hidden");
-    } else {
-      this.genreChipTarget.classList.remove("hidden");
-    }
+    this.#updateSelection(this.genreDropdownTarget, this.genreChipTarget, 3);
   }
 
   clearGenre() {
-    // genreDropdown is an ul, each elem should be a li
-    Array.from(this.genreDropdownTarget.children).forEach(li => {
+    this.#clearSelection(this.genreDropdownTarget, this.genreChipTarget);
+  }
+
+  #updateSelection(dropdown, chip, showLength = 3) {
+    const selections = Array.from(dropdown.children).reduce((acc, li) => {
+      const container = li.firstElementChild;
+      const checkbox = container.firstElementChild;
+      const label = container.lastElementChild;
+      if (checkbox.checked) acc.push(label.innerText.trim());
+      return acc;
+    }, []);
+    console.log(selections)
+    let str = "";
+    if (selections.length > showLength) {
+      const remainingLength = selections.length - showLength;
+      str = `${selections.slice(0, showLength).join(", ")}, +${remainingLength}`;
+    } else if (selections.length > 0) {
+      str = selections.join(", ");
+    }
+    str = str.trim()
+    const display = chip.querySelector(".track-filter-chip-text");
+    display.innerText = str;
+
+    if (str === "") {
+      chip.classList.add("hidden");
+    } else {
+      chip.classList.remove("hidden");
+    }
+  }
+
+  #clearSelection(dropdown, chip) {
+    // assumes dropdown is an ul, each elem should be a li
+    Array.from(dropdown.children).forEach(li => {
       const container = li.firstElementChild;
       const checkbox = container.firstElementChild;
       checkbox.checked = false;
     });
 
-    this.genreChipTarget.classList.add("hidden");
+    chip.classList.add("hidden");
   }
 }
