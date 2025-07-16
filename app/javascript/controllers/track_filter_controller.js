a8fec8cb55c624380ab17887ef1f0b6a80366a50
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-15 21:34:12 -0400

    add genre query

---
+++ app/javascript/controllers/track_filter_controller.js
@@ -0,0 +1,46 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="track-filter"
+export default class extends Controller {
+  static targets = [
+    "genreDropdown",
+    "genreChip"
+  ];
+
+  connect() {
+    this.updateGenre();
+  }
+
+  updateGenre() {
+    const selectedGenres = Array.from(this.genreDropdownTarget.children).reduce((genres, li) => {
+      const container = li.firstElementChild;
+      const checkbox = container.firstElementChild;
+      const label = container.lastElementChild;
+      if (checkbox.checked) genres.push(label.innerText);
+      return genres;
+    }, []);
+
+    let genreStr = "";
+    if (selectedGenres.length > 3) {
+      const remainingLength = selectedGenres.length - 3;
+      genreStr = `${selectedGenres.slice(0, 3).join(", ")}, +${remainingLength}`;
+    } else if (selectedGenres.length > 0) {
+      genreStr = selectedGenres.join(", ");
+    }
+
+    const genreDisplay = this.genreChipTarget.querySelector(".track-filter-chip-text");
+    genreDisplay.innerText = genreStr;
+    this.genreChipTarget.classList.remove("hidden");
+  }
+
+  clearGenre() {
+    // genreDropdown is an ul, each elem should be a li
+    Array.from(this.genreDropdownTarget.children).forEach(li => {
+      const container = li.firstElementChild;
+      const checkbox = container.firstElementChild;
+      checkbox.checked = false;
+    });
+
+    this.genreChipTarget.classList.add("hidden");
+  }
+}
