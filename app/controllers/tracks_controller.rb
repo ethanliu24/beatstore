bea5ea4a33a9925eec1e1afc0b69573e6c966ad0
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-18 12:08:39 -0400

    add loader

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,9 +1,8 @@
 class TracksController < ApplicationController
   def index
-    # TODO pagination
     base_scope = Track.where(is_public: true)
     @q = base_scope.ransack(params[:q], auth_object: current_user)
-    queried_tracks = @q.result.includes(:tags)
+    queried_tracks = @q.result.includes(:tags).order(created_at: :desc)
     @pagy, @tracks = pagy_keyset(queried_tracks, limit: 20)
 
     if turbo_or_xhr_request?
