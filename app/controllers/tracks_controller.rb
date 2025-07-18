2b7eb077c0ece8b3df5d4e046e2ae59f43357b7b
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-18 12:16:36 -0400

    add dynamic ordering for keyset

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,7 +2,18 @@ class TracksController < ApplicationController
   def index
     base_scope = Track.where(is_public: true)
     @q = base_scope.ransack(params[:q], auth_object: current_user)
-    queried_tracks = @q.result.includes(:tags).order(created_at: :desc)
+
+    # need ordering for pagy_keyset, but there are dynamic order queries
+    sorts = if @q.sorts.any?
+      @q.sorts.map do |s|
+        direction = s.dir.downcase.to_sym
+        [s.name.to_sym, direction]
+      end.to_h
+    else
+      { created_at: :desc, id: :desc }
+    end
+
+    queried_tracks = @q.result.includes(:tags).reorder(sorts)
     @pagy, @tracks = pagy_keyset(queried_tracks, limit: 20)
 
     if turbo_or_xhr_request?
