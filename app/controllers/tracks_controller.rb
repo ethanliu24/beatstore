364a86b82dcf505ea3b01e554c09076646c48df3
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-18 13:44:38 -0400

    remove order functionality in filter for infinite scroll track page

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,18 +2,7 @@ class TracksController < ApplicationController
   def index
     base_scope = Track.where(is_public: true)
     @q = base_scope.ransack(params[:q], auth_object: current_user)
-
-    # need ordering for pagy_keyset, but there are dynamic order queries
-    sorts = if @q.sorts.any?
-      @q.sorts.map do |s|
-        direction = s.dir.downcase.to_sym
-        [s.name.to_sym, direction]
-      end.to_h
-    else
-      { created_at: :desc, id: :desc }
-    end
-
-    queried_tracks = @q.result.includes(:tags).reorder(sorts)
+    queried_tracks = @q.result.includes(:tags).order(created_at: :desc)
     @pagy, @tracks = pagy_keyset(queried_tracks, limit: 20)
 
     respond_to do |format|
