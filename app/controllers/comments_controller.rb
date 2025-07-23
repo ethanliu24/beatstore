b41c1cf374e675537ffec6119cf9beabd8a716d3
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-23 17:47:21 -0400

    add comment cntrler

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,48 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_comment, except: [ :create ]
+  before_action :user_can_manage?, except: [ :create ]
+
+  def create
+    entity_class = params[:comment][:entity_type].constantize
+    entity = entity_class.find(params[:comment][:entity_id])
+
+    @comment = Comment.create!(
+      content: params[:comment][:content],
+      entity:,
+      user: current_user
+    )
+  end
+
+  def update
+    @comment.update!(params.require(:comment).permit(:content))
+  end
+
+  def destroy
+    @comment.destroy!
+  end
+
+  private
+
+  def user_can_manage?
+    unless current_user.admin? || current_user.id == @comment.user.id
+      head :unauthorized
+    end
+  end
+
+  def sanitize_comment_params
+    params.require(:comment).permit(
+      :content,
+      :entity_type,
+      :entity_id,
+    )
+  end
+
+  def set_comment
+    @comment = if current_user.admin?
+      Comment.find(params[:id])
+    else
+      current_user.comments.find(params[:id])
+    end
+  end
+end
---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,152 @@
+require 'rails_helper'
+
+RSpec.describe "Comments", type: :request do
+  let!(:track) { create(:track) }
+  let(:params) {
+    {
+      content: "Comment.",
+      entity_type: "Track",
+      entity_id: track.id
+    }
+  }
+
+  describe "current user is logged in" do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user, scope: :user
+    end
+
+    it "should create a comment with the current user" do
+      expect {
+        post comments_url, params: { comment: params.merge(foo: "bar") }
+      }.to change(Comment, :count).by(1)
+
+      comment = Comment.last
+      expect(response).to have_http_status(204)
+      expect(comment.content).to eq("Comment.")
+      expect(comment.entity_type).to eq("Track")
+      expect(comment.entity_id).to eq(track.id)
+      expect(comment.user_id).to eq(user.id)
+      expect(comment.respond_to?(:foo)).to be false
+    end
+
+    it "should update a comment's content only made by the current user" do
+      expect {
+        post comments_url, params: { comment: params }
+      }.to change(Comment, :count).by(1)
+
+      comment = Comment.last
+      expect(comment.content).to eq("Comment.")
+      expect(comment.entity_type).to eq("Track")
+      expect(comment.entity_id).to eq(track.id)
+      expect(comment.user_id).to eq(user.id)
+
+      put comment_url(comment), params: { comment: { content: "updated", foo: "bar" } }
+
+      comment.reload
+      expect(response).to have_http_status(204)
+      expect(comment.content).to eq("updated")
+      expect(comment.entity_type).to eq("Track")
+      expect(comment.entity_id).to eq(track.id)
+      expect(comment.user_id).to eq(user.id)
+      expect(comment.respond_to?(:foo)).to be false
+    end
+
+    it "should delete the comment made by the current user" do
+      expect {
+        post comments_url, params: { comment: params }
+      }.to change(Comment, :count).by(1)
+
+      comment = Comment.last
+      expect(comment.content).to eq("Comment.")
+
+      expect {
+        delete comment_url(comment)
+      }.to change(Comment, :count).by(-1)
+
+      expect(response).to have_http_status(204)
+    end
+  end
+
+  describe "the user taking action is a admin" do
+    let(:user) { create(:user) }
+    let(:admin) { create(:admin) }
+    let!(:comment) { create(:comment, entity: track, user: user) }
+
+    before do
+      sign_in admin, scope: :user
+    end
+
+    it "should let admin update a comment" do
+      put comment_url(comment), params: { comment: { content: "updated by admin" } }
+
+      comment.reload
+      expect(response).to have_http_status(204)
+      expect(comment.content).to eq("updated by admin")
+      expect(comment.entity_type).to eq("Track")
+      expect(comment.entity_id).to eq(track.id)
+      expect(comment.user_id).to eq(user.id)
+    end
+
+    it "should let admin delete a comment" do
+      expect {
+        delete comment_url(comment)
+      }.to change(Comment, :count).by(-1)
+
+      expect(response).to have_http_status(204)
+    end
+  end
+
+  describe "a user that didn't create the comment" do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user, email: "comments_other_user@email.com", username: "comments_other_user") }
+    let!(:comment) { create(:comment, entity: track, user: user) }
+
+    before do
+      sign_in other_user, scope: :user
+    end
+
+    it "shuold not be able to update the comment" do
+      put comment_url(comment)
+      expect(response).to have_http_status(404)
+    end
+
+    it "should not be able to delete a comment" do
+      expect {
+        delete comment_url(comment)
+      }.not_to change(Comment, :count)
+
+      expect(response).to have_http_status(404)
+    end
+  end
+
+  describe "user is not logged in" do
+    it "should not let the user create a comment" do
+      expect {
+        post comments_url, params: { comment: params }
+      }.not_to change(Comment, :count)
+
+      expect(response).to have_http_status(302)
+    end
+
+    it "should not let the user update a comment" do
+      user = create(:user)
+      comment = create(:comment, entity: track, user: user)
+
+      put comment_url(comment)
+      expect(response).to have_http_status(302)
+    end
+
+    it "should not let the user delete a comment" do
+      user = create(:user)
+      comment = create(:comment, entity: track, user: user)
+
+      expect {
+        delete comment_url(comment)
+      }.not_to change(Comment, :count)
+
+      expect(response).to have_http_status(302)
+    end
+  end
+end
