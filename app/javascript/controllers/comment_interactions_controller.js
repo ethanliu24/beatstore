41c3a1be84ecc255bd96f417eaacab7011cbd952
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-26 21:01:33 -0400

    add dislike interaction

--- app/javascript/controllers/comment_interactions_controller.js
+++ app/javascript/controllers/comment_interactions_controller.js
@@ -10,15 +10,14 @@ export default class extends Controller {
   static values = { authorized: Boolean };
 
   connect() {
-    console.log(this.authorizedValue)
     if (this.#loggedIn()) {
       /** Assumes that interaction target is a button and its wrapped inside a form,
        *  as to button_to tag is this structure.
        */
       this.likeBtn = this.likeTarget.parentElement;
-      // this.dislikeBtn = this.dislikeTarget.parentElement;
+      this.dislikeBtn = this.dislikeTarget.parentElement;
       this.unlikeBtn = this.unlikeTarget.parentElement;
-      // this.undislikeBtn = this.undislikeTarget.parentElement;
+      this.undislikeBtn = this.undislikeTarget.parentElement;
     }
   }
 
@@ -28,14 +27,30 @@ export default class extends Controller {
     this.likeCountTarget.innerText = `${this.#getLikeCount() + 1}`;
   }
 
+  dislike() {
+    this.dislikeBtn.classList.add("hidden");
+    this.undislikeBtn.classList.remove("hidden");
+    this.dislikeCountTarget.innerText = `${this.#getDislikeCount() + 1}`;
+  }
+
   unlike() {
     this.unlikeBtn.classList.add("hidden");
     this.likeBtn.classList.remove("hidden");
     this.likeCountTarget.innerText = `${this.#getLikeCount() - 1}`;
   }
 
+  undislike() {
+    this.undislikeBtn.classList.add("hidden");
+    this.dislikeBtn.classList.remove("hidden");
+    this.dislikeCountTarget.innerText = `${this.#getDislikeCount() - 1}`;
+  }
+
   #getLikeCount() {
     return parseInt(this.likeCountTarget.innerText, 10);
+  }
+
+  #getDislikeCount() {
+    return parseInt(this.dislikeCountTarget.innerText, 10);
   }
 
   #loggedIn() {
