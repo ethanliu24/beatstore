d737edd7e9dc68a4142b201ffd3fb7e4c544a53c
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-28 12:56:41 -0400

    add track download controller

---
+++ app/controllers/downloads_controller.rb
@@ -0,0 +1,28 @@
+class DownloadsController < ApplicationController
+  before_action :set_track
+
+  def free_download
+    unless file_exists?(@track.tagged_mp3)
+      head :not_found
+    end
+
+    send_data @track.tagged_mp3.download,
+      filename: set_file_name(@track, "mp3"),
+      type: "mp3",
+      disposition: "attachment"
+  end
+
+  private
+
+  def set_file_name(track, extension)
+    "#{track.title} - #{track.bpm}bpm #{track.key.downcase}.#{extension}"
+  end
+
+  def file_exists?(file)
+    file.attached?
+  end
+
+  def set_track
+    @track = Track.find(params[:id])
+  end
+end
