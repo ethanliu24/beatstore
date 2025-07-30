c9534ddb81f6eda05fc8126eb3d493773a708f4e
ethanliu24 <liu.ethan2005@gmail.com>
2025-07-29 21:49:18 -0400

    move to service

---
+++ app/services/tracks/find_similar_tracks_service.rb
@@ -0,0 +1,40 @@
+# frozen_literal_string: true
+
+module Tracks
+  class FindSimilarTracksService
+    SIMILAR_TRACKS_BPM_RANGE = 10
+
+    def initialize(track:)
+      @track = track
+    end
+
+    def call
+      Track.find_by_sql([
+        <<~SQL,
+          SELECT
+            tracks.*,
+            COUNT(tags.name) AS tag_match_count,
+            (CASE WHEN tracks.genre = ? THEN 1 ELSE 0 END) AS genre_match
+          FROM tracks
+          LEFT JOIN track_tags AS tags ON tags.track_id = tracks.id
+          WHERE
+            tracks.id != ?
+            AND tracks.is_public IS TRUE
+            AND (
+              tracks.genre = ?
+              OR tracks.bpm BETWEEN ? AND ?
+              OR tags.name IN (?)
+            )
+          GROUP BY tracks.id
+          ORDER BY tag_match_count DESC, genre_match DESC, tracks.created_at DESC
+        SQL
+          @track.genre,
+          @track.id,
+          @track.genre,
+          @track.bpm - SIMILAR_TRACKS_BPM_RANGE,
+          @track.bpm + SIMILAR_TRACKS_BPM_RANGE,
+          Array(@track.tags.pluck(:name))
+      ])
+    end
+  end
+end
