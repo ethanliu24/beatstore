2e02251f4c0045e1ea032df2eaff7cce77b84973
ethanliu24 <liu.ethan2005@gmail.com>
2025-08-04 21:02:25 -0400

    disable scrolling when modals open

--- app/javascript/controllers/modal_manager_controller.js
+++ app/javascript/controllers/modal_manager_controller.js
@@ -5,9 +5,11 @@ export default class extends Controller {
   static targets = ["modal"];
 
   connect() {
+    document.body.classList.add("overflow-hidden");
   }
 
   close() {
+    document.body.classList.remove("overflow-hidden");
     this.element.remove();
   }
 
