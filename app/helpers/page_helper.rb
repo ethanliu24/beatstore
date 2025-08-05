ff84e44d2cda7d81717642ae39214560850ea0e5
ethanliu24 <liu.ethan2005@gmail.com>
2025-08-05 13:39:13 -0400

    change admin bg color and layout

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -2,8 +2,6 @@ module PageHelper
   def current_page
     if devise_controller?
       "layouts/auth_main"
-    elsif controller_path == "admin/tracks" && (action_name == "new" || action_name == "edit")
-      "layouts/default_main"
     elsif controller_path.start_with?("admin/")
       "layouts/admin_main"
     else
