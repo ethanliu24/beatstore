997cfa193b3cb59f7946003540472822d45b73c1
ethanliu24 <liu.ethan2005@gmail.com>
2025-08-11 22:18:03 -0400

    model track collaborator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_26_043925) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_12_020645) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_26_043925) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "collaborators", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "role", null: false
+    t.decimal "profit_share", precision: 5, scale: 2, default: "0.0", null: false
+    t.decimal "publishing_share", precision: 5, scale: 2, default: "0.0", null: false
+    t.text "notes"
+    t.bigint "track_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_collaborators_on_track_id"
   end
 
   create_table "comment_interactions", force: :cascade do |t|
@@ -128,6 +140,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_26_043925) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "collaborators", "tracks"
   add_foreign_key "comment_interactions", "comments"
   add_foreign_key "comment_interactions", "users", on_delete: :nullify
   add_foreign_key "comments", "users"
