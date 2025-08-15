cb0178db609aa191339b0153fb370305801591a9
ethanliu24 <liu.ethan2005@gmail.com>
2025-08-15 15:40:09 -0400

    default artist and links to empty strs

--- db/migrate/20250815034925_model_samples_for_track.rb
+++ db/migrate/20250815034925_model_samples_for_track.rb
@@ -2,8 +2,8 @@ class ModelSamplesForTrack < ActiveRecord::Migration[8.0]
   def change
     create_table :samples do |t|
       t.string :name, null: false
-      t.string :artist, null: true
-      t.string :link, null: true
+      t.string :artist, default: ""
+      t.string :link, default: ""
       t.references :track, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -78,8 +78,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_15_034925) do
 
   create_table "samples", force: :cascade do |t|
     t.string "name", null: false
-    t.string "artist"
-    t.string "link"
+    t.string "artist", default: ""
+    t.string "link", default: ""
     t.bigint "track_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
