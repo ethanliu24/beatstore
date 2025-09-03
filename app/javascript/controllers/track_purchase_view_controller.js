09d0b7a28217874e1583b0cc806df9381402abbe
ethanliu24 <liu.ethan2005@gmail.com>
2025-09-03 19:09:30 -0400

    add js to add track to controller

--- app/javascript/controllers/track_purchase_view_controller.js
+++ app/javascript/controllers/track_purchase_view_controller.js
@@ -3,11 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="track-purchase-view"
 export default class extends Controller {
   static values = {
-
+    createCartItemUrl: String,
+    initialLicenseId: Number,
+    trackId: Number,
+    productType: String
   }
 
   connect() {
-    this.selectedLicenseId = null;
+    this.selectedLicenseId = this.initialLicenseIdValue;
+    this.quantity = 1;
   }
 
   selectLicense(e) {
@@ -15,6 +19,22 @@ export default class extends Controller {
   }
 
   addToCart() {
+    if (!this.selectedLicenseIdValue) return;
 
+    fetch(this.createCartItemUrlValue, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": document.querySelector("[name='csrf-token']").content,
+        "Accept": "text/vnd.turbo-stream.html"
+      },
+      body: {
+        product_id: this.trackIdValue,
+        product_type: this.productTypeValue,
+        license_id: this.selectedLicenseId,
+        quantity: this.quantity
+      }
+    })
+      .then(r => r.text())
+      .then(html => Turbo.renderStreamMessage(html))
   }
 }
