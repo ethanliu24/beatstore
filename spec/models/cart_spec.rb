5b0757e10d18c7d6566f0338de85e4976d5d7695
ethanliu24 <liu.ethan2005@gmail.com>
2025-09-04 16:19:20 -0400

    add tests for total cents

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe Cart, type: :model do
   describe "associations" do
     it { should belong_to(:user).required }
-    it { should have_many(:items) }
+    it { should have_many(:cart_items) }
   end
 
   describe "dependent behavior" do
@@ -34,6 +34,30 @@ RSpec.describe Cart, type: :model do
       track = create(:track)
 
       expect(cart.has_item?(license_id: license.id, product_type: Track.name, product_id: track.id)).to be(false)
+    end
+  end
+
+  describe "#total_cents" do
+    it "should return the correct total" do
+      cart = create(:cart)
+      license1 = create(:license, price_cents: 1000)
+      license2 = create(:license, title: "L2", price_cents: 1234)
+      track = create(:track)
+      create(:cart_item, cart:, product: track, license: license1)
+      create(:cart_item, cart:, product: track, license: license2)
+
+      expect(cart.total_price_cents).to eq(2234)
+    end
+
+    it "should not have floating point errors" do
+      cart = create(:cart)
+      license1 = create(:license, price_cents: 0001)
+      license2 = create(:license, title: "L2", price_cents: 0002)
+      track = create(:track)
+      create(:cart_item, cart:, product: track, license: license1)
+      create(:cart_item, cart:, product: track, license: license2)
+
+      expect(cart.total_price_cents).to eq(0003)
     end
   end
 end
