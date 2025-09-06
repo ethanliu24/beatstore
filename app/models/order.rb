fb94a1b3d338f5feac2b0dfcabb39d51a9831a16
ethanliu24 <liu.ethan2005@gmail.com>
2025-09-06 13:38:43 -0400

    add orders model

---
+++ app/models/order.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Order < ApplicationRecord
+  belongs_to :user
+  has_many :order_items, dependent: :restrict_with_error
+
+  enum status: {
+    pending: "pending",
+    paid: "paid",
+    failed: "failed"
+  }
+
+  validates :currency, presence: true
+  validates :subtotal_cents, numericality: { greater_than_or_equal_to: 0 }
+
+  before_update :prevent_core_changes_if_paid
+
+  private
+
+  # enforce immutability after payment
+  def prevent_core_changes_if_paid
+    if paid? && (changed & %w[total_cents currency user_id]).any?
+      errors.add(:base, "Cannot modify order details once paid")
+      throw(:abort)
+    end
+  end
+end
