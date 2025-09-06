398c8a00f1aaa87a99e0f2e3516beb91e1a39d1b
ethanliu24 <liu.ethan2005@gmail.com>
2025-09-06 00:20:58 -0400

    add item photo and info

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -6,6 +6,8 @@ module CartHelper
   end
 
   def cart_item_has_photo?(cart_item)
+    return unless cart_item.available?
+
     case cart_item.product_type
     when Track.name
       cart_item.product.cover_photo.attached?
@@ -15,6 +17,8 @@ module CartHelper
   end
 
   def cart_item_photo_url(cart_item)
+    return unless cart_item.available?
+
     product = cart_item.product
 
     case cart_item.product_type
@@ -26,6 +30,8 @@ module CartHelper
   end
 
   def cart_item_product_title(cart_item)
+    return unless cart_item.available?
+
     case cart_item.product_type
     when Track.name
       track = cart_item.product
@@ -33,5 +39,17 @@ module CartHelper
     else
       ""
     end
+  end
+
+  def cart_item_track_files_delivered(cart_item)
+    return unless cart_item.available?
+    return nil unless cart_item.product_type == Track.name
+
+    files = []
+    contract = cart_item.license.contract
+    files << "mp3" if contract[:delivers_mp3]
+    files << "wav" if contract[:delivers_wav] || contract[:delivers_stems]
+
+    files.join(", ")
   end
 end
