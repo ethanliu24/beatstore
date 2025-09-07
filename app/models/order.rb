76b4c2038e2aae747c18c44656a9d5c92caa824f
ethanliu24 <liu.ethan2005@gmail.com>
2025-09-07 16:51:27 -0400

    add comments

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This should be partly immutable except for status updates. Once transaction clears it should be readonly
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :restrict_with_error
