985aa7c1b10fae6985a86b4fb9fc3a8aa7cb4fa9
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2025-09-15 23:54:14 -0400

    Don't load Stripe configuration if not present (#246)

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,5 +5,9 @@ STRIPE_CONFIG = Rails.application.credentials.dig(
   Rails.env.production? ? :live : :test
 )
 
-Stripe.api_key = STRIPE_CONFIG[:secret_key]
-STRIPE_PAYMENTS_WEBHOOK_SECRET = STRIPE_CONFIG[:payments_webhook_secret]
+# We'll have to skip Stripe configs in cases such as dependabot PRs,
+# because dependabots cannot access project secrets so it fails
+if STRIPE_CONFIG.present?
+  Stripe.api_key = STRIPE_CONFIG[:secret_key]
+  STRIPE_PAYMENTS_WEBHOOK_SECRET = STRIPE_CONFIG[:payments_webhook_secret]
+end
