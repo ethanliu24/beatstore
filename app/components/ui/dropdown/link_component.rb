28f17bfb6640b3f0d6ca8b72a09c7d186ceb11c8
ethanliu24 <liu.ethan2005@gmail.com>
2025-09-17 12:38:28 -0400

    refactor sidebar and link component

--- app/components/ui/dropdown/link_component.rb
+++ app/components/ui/dropdown/link_component.rb
@@ -3,18 +3,19 @@
 module Ui
   module Dropdown
     class LinkComponent < ApplicationComponent
-      def initialize(url:, text:, icon:, icon_size:, classes: "", data: {})
+      def initialize(url:, text:, icon:, icon_size:, classes: "", text_classes: "", data: {})
         @url = url
         @text = text
         @icon_name = icon
         @icon_size = icon_size
         @classes = classes
         @data = data
+        @text_classes = text_classes
       end
 
       private
 
-      attr_reader :url, :text, :icon_name, :classes, :data
+      attr_reader :url, :text, :icon_name, :classes, :text_classes, :data
 
       def icon_size
         "w-#{@icon_size} h-#{@icon_size} aspect-square"
