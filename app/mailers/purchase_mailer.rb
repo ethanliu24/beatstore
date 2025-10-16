5149e2a97f8fc90fdf7d5e64aef8bce6cf158e06
ethanliu24 <liu.ethan2005@gmail.com>
2025-10-16 19:33:36 -0400

    add purchase complete email

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,7 +1,14 @@
 class PurchaseMailer < ApplicationMailer
-  def purchase_complete_email
+  def purchase_complete
     @user = params[:user]
     @order = params[:order]
-    # mail(to: @user.email, subject: "")
+    @transaction = @order.payment_transaction
+
+    subject = "Thank you for your purchase"
+    mail(to: @user.email, subject:)
+
+    if @transaction.customer_email.present?
+      mail(to: @transaction.customer_email, subject:)
+    end
   end
 end
--- spec/mailers/previews/purchase_mailer_preview.rb
+++ spec/mailers/previews/purchase_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/purchase_mailer
 class PurchaseMailerPreview < ActionMailer::Preview
+  def purchase_complete
+    user = User.first || User.new(email: "purchase_complete@example.com", display_name: "Test User")
+    order = Order.last || Order.new
 
+    PurchaseMailer.with(user: user, order: order).purchase_complete
+  end
 end
