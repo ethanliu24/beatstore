bf382534a35224b0df34738835e57a04dcd5f34e
ethanliu24 <liu.ethan2005@gmail.com>
2025-11-10 19:25:58 -0500

    add back js license fetch

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -145,6 +145,21 @@ export default class extends Controller {
     this.stopPropagation(e);
   }
 
+  async fetchTrackPurchaseModal(e) {
+    e.stopPropagation();
+    const url = e.currentTarget.dataset.trackPurchaseModalUrl;
+    await fetch(url, {
+      method: "GET",
+      headers: {
+        "X-CSRF-Token": document.querySelector("[name='csrf-token']").content,
+        "Accept": "text/vnd.turbo-stream.html"
+      }
+    })
+      .then(r => r.text())
+      .then(html => Turbo.renderStreamMessage(html))
+      .then();
+  }
+
   async downloadTrack() {
     try {
       const url = `/download/track/${this.currentTrackId}/free`;
@@ -204,6 +219,7 @@ export default class extends Controller {
       method: "GET",
       headers: {
         "X-CSRF-Token": document.querySelector("[name='csrf-token']").content,
+        "Accept": "text/vnd.turbo-stream.html",
       }
     })
       .then(res => {
