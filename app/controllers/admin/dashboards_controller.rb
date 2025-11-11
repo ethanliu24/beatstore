06961ab1c3a99753528d2e10e114cadb3e6156d6
ethanliu24 <liu.ethan2005@gmail.com>
2025-11-11 16:33:42 -0500

    add quick stats panel

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,7 +3,7 @@
 module Admin
   class DashboardsController < Admin::BaseController
     def show
-      @total_stats, @chrono_stats = CrunchAdminAnalyticsService.new(WindowSize::ONE_YEAR).call
+      @cum_stats, @chrono_stats = CrunchAdminAnalyticsService.new(WindowSize::ONE_YEAR).call
     end
   end
 end
