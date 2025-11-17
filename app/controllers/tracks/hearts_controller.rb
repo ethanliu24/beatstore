c9b28b7946e6b33a3bfd15256bb55ded7b1dbf3f
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2025-11-17 18:04:06 -0500

    Update like icons via turbo stream (#301)
    * use turbo stream to update heart icon for show
    
    * update track more dropdown
    
    * fix tests

--- app/controllers/tracks/hearts_controller.rb
+++ app/controllers/tracks/hearts_controller.rb
@@ -2,15 +2,21 @@ class Tracks::HeartsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    track = Track.find(params[:track_id])
-    current_user.hearts.create(track: track)
-    head :no_content
+    @track = Track.find(params[:track_id])
+    current_user.hearts.create(track: @track)
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def destroy
-    track = Track.find(params[:track_id])
-    heart = current_user.hearts.find_by(track: track)
+    @track = Track.find(params[:track_id])
+    heart = current_user.hearts.find_by(track: @track)
     heart&.destroy
-    head :no_content
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 end
