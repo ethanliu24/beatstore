5f44516450aaba454acfba2176016b5447e55763
ethanliu24 <liu.ethan2005@gmail.com>
2025-12-15 22:57:26 -0500

    add clickable search

--- app/components/tag_component.rb
+++ app/components/tag_component.rb
@@ -2,10 +2,11 @@
 
 class TagComponent < ApplicationComponent
   def initialize(name:)
+    @tag_name = name
     @tag = "##{name}"
   end
 
   private
 
-  attr_reader :tag
+  attr_reader :tag, :tag_name
 end
