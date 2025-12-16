9f6da393f1f32998603a7eab8d0a22d88dee2252
ethanliu24 <liu.ethan2005@gmail.com>
2025-12-16 15:23:34 -0500

    update download modal to take in cusotmer data

--- app/models/free_download.rb
+++ app/models/free_download.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class FreeDownload < ApplicationRecord
+  validates :email, presence: true
+  validates :customer_name, presence: true
+
   belongs_to :user, optional: true
   belongs_to :track, optional: true
 end
