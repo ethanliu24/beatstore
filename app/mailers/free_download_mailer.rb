a9053a8aa365c9ca5b8e7641ddd067ad5b97db90
ethanliu24 <liu.ethan2005@gmail.com>
2025-12-17 21:55:19 -0500

    add email content

--- app/mailers/free_download_mailer.rb
+++ app/mailers/free_download_mailer.rb
@@ -2,8 +2,9 @@ class FreeDownloadMailer < ApplicationMailer
   def download
     @free_download = params[:free_download]
     @track = @free_download.track
+    @free_license = @track.licenses.kept.find { |l| l.price_cents == 0 }
 
-    subject = "Your Free Download - #{track.title}"
+    subject = "Your Download - #{@track.title}"
 
     recipients = [ @free_download.email ]
     mail(to: recipients, subject:)
