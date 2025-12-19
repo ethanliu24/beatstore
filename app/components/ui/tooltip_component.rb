cb6d2740e3759e6aac8622fe0f1508d1d200acd1
ethanliu24 <liu.ethan2005@gmail.com>
2025-12-19 13:27:28 -0500

    add color configs for tooltip

--- app/components/ui/tooltip_component.rb
+++ app/components/ui/tooltip_component.rb
@@ -4,13 +4,33 @@ module Ui
   class TooltipComponent < ApplicationComponent
     renders_one :body
 
-    def initialize(anchor_id:, position:)
+    def initialize(anchor_id:, position: :top, color_pallet: nil)
       @anchor_id = anchor_id
       @position = position  # valid values: top, left, right, bottom, defaults to top
+      @bg_color = get_bg_color(color_pallet:)
+      @text_color = get_text_color(color_pallet:)
     end
 
     private
 
-    attr_reader :anchor_id, :position
+    attr_reader :anchor_id, :position, :bg_color, :text_color
+
+    def get_bg_color(color_pallet:)
+      case color_pallet
+      when "accent"
+        "accent"
+      else
+        "tooltip-bg"
+      end
+    end
+
+    def get_text_color(color_pallet:)
+      case color_pallet
+      when "accent"
+        "white"
+      else
+        "tooltip-text"
+      end
+    end
   end
 end
