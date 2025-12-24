f3e09c89b3383d3c1378737d10395c73ca088d13
ethanliu24 <liu.ethan2005@gmail.com>
2025-12-24 00:44:48 -0500

    add queue scope to track list

--- app/javascript/controllers/audio_queue_controller.js
+++ app/javascript/controllers/audio_queue_controller.js
@@ -3,20 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="audio-queue"
 export default class extends Controller {
   connect() {
-    this.queue = new Map();
+    this.queue = [];
     this.activeScopeCtx = null;
   }
 
   updateQueue(queueScope) {
     if (this.activeScopeCtx !== queueScope) {
-      this.queue = new Map();
       const tracks = document.querySelectorAll(`[data-queue-scope="${queueScope}"]`);
       this.activeScopeCtx = queueScope;
 
-      Array.from(tracks).forEach((el, cursor) => {
-        this.queue.set(cursor, {
+      this.queue = Array.from(tracks).map((el, cursor) => {
+        return {
           trackId: el.dataset.trackId
-        });
+        };
       });
     }
   }
