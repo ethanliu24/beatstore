0cdab729df6ec99df3999167564c7f7cc93166cd
ethanliu24 <liu.ethan2005@gmail.com>
2025-12-25 23:50:36 -0800

    get track cursor helper

--- app/javascript/controllers/audio_queue_controller.js
+++ app/javascript/controllers/audio_queue_controller.js
@@ -25,26 +25,48 @@ export default class extends Controller {
     });
   }
 
-  pickTrack(mode, cursor) {
+  getTrackCursor(trackId) {
+    this.queue.forEach((t) => {
+      if (t.trackId === trackId) {
+        return t.cursor;
+      }
+    });
+
+    return null;
+  }
+
+  pickTrack(mode, trackId) {
+    const cursor = this.getTrackCursor(trackId);
+    if (!cursor) return null;
+
     switch (mode) {
       case PlayerModes.NEXT:
         return this.pickNextTrack(cursor);
       case PlayerModes.REPEAT:
-        return this.queue.at(cursor).trackId;
+        return this.pickCurrentTrack(cursor);
       case PlayerModes.SHUFFLE:
         return this.pickNextTrackShuffle(cursor);
       default:
         console.error(`Unknown player mode: ${mode}`);
+        return null;
     }
   }
 
   pickNextTrack(cursor) {
+    if (!this.queue) return null;
+
     const nextIndex = (cursor + 1) % this.queue.length;
     const track = this.queue.at(nextIndex);
     return track.trackId;
   }
 
+  pickCurrentTrack(cursor) {
+    return !this.queue? null : this.queue.at(cursor).trackId;
+  }
+
   pickNextTrackShuffle(current) {
+    if (!this.queue) return null;
+
     const selections = [...this.queue].splice(current, 1);
     const track = selections[Math.floor(Math.random() * this.queue.length)];
     return track.trackId;
