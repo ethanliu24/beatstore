0f050ab504fce762bb890a150320fcd5db4e1fd4
ethanliu24 <liu.ethan2005@gmail.com>
2026-01-16 14:49:18 -0500

    add safeguards to email creds

--- app/lib/credentials/email.rb
+++ app/lib/credentials/email.rb
@@ -6,10 +6,14 @@ module Credentials
     include CredentialResolver
 
     def producer_email
+      return "producer@example.com" if Rails.env.test?
+
       resolve_credential(:email, :producer, namespace:)
     end
 
     def domain_email
+      return "domain@example.com" if Rails.env.test?
+
       resolve_credential(:email, :domain, app_env, namespace:)
     end
 
