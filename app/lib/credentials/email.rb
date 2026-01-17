e6b93de91c50b8026a90b10bdb8b41695ed52345
ethanliu24 <liu.ethan2005@gmail.com>
2026-01-16 23:40:44 -0500

    safe guard dependabot only

--- app/lib/credentials/email.rb
+++ app/lib/credentials/email.rb
@@ -6,13 +6,13 @@ module Credentials
     include CredentialResolver
 
     def producer_email
-      return "producer@example.com" if ENV["CI"].present?
+      return "producer@example.com" if dependabot?
 
       resolve_credential(:email, :producer, namespace:)
     end
 
     def domain_email
-      return "domain@example.com" if ENV["CI"].present?
+      return "domain@example.com" if dependabot?
 
       resolve_credential(:email, :domain, app_env, namespace:)
     end
@@ -25,6 +25,10 @@ module Credentials
 
     def app_env
       Rails.env.production? ? :prod : :dev
+    end
+
+    def dependabot?
+      ENV["DEPENDABOT"].to_s.downcase == "true"
     end
   end
 end
