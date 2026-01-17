a0f7ca1e29bf846f3e4c44d2f4d3ada31d7854f4
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2026-01-16 23:19:19 -0500

    Revert "Move credential digs outside of initializers (#382)"
    This reverts commit e4c26786d7240c2afca15523ae512c83c8d63de0.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: -> { ::Credentials::Email.domain_email }
+  # TODO change to domain name
+  default from: "from@example.com"
   layout "mailer"
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -8,11 +8,6 @@ class ContactMailer < ApplicationMailer
 
     sender_email = inbound_email.email
 
-    mail(
-      subject: @subject,
-      to: ::Credentials::Email.producer_email,
-      reply_to: sender_email,
-      cc: ::Credentials::Email.domain_email
-    )
+    mail(subject: @subject, to: PRODUCER_EMAIL, reply_to: sender_email, cc: DOMAIN_EMAIL)
   end
 end
