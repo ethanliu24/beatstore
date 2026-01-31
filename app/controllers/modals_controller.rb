fac256f8c8ffdee425d44e3c2b77a9107248b66d
ethanliu24 <liu.ethan2005@gmail.com>
2026-01-31 13:03:58 -0500

    fix data retrival issue in modal

--- app/controllers/modals_controller.rb
+++ app/controllers/modals_controller.rb
@@ -90,7 +90,7 @@ class ModalsController < ApplicationController
   def free_download
     track = Track.kept.find(params[:id])
     free_download = FreeDownload.new
-    download_info = session[:free_download_info] || {}
+    download_info = (session[:free_download_info] || {}).deep_symbolize_keys
     render_modal(partial: "modals/free_download", locals: { track:, free_download:, download_info: })
   end
 
