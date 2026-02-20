8d662e28749438f5325fd53f6c08f0364e190d2e
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2026-02-19 23:23:32 -0500

    Make playable cover photo take explicit size (#440)
    * make cover photo take rem
    
    * fix dynamic photo sizing
    
    * modify cover photo sizes
    
    * remove comment
    
    * make unit configurable
    
    * fix tests

--- app/components/tracks/playable_cover_photo.rb
+++ app/components/tracks/playable_cover_photo.rb
@@ -2,9 +2,10 @@
 
 module Tracks
   class PlayableCoverPhoto < ApplicationComponent
-    def initialize(track:, size:)
+    def initialize(track:, size:, unit: "rem")
       @track = track
-      @size = size
+      @size = size.to_f
+      @unit = unit
     end
 
     private
@@ -12,7 +13,9 @@ module Tracks
     attr_reader :track
 
     def image_size
-      "w-#{@size} h-#{@size} min-w-#{@size} min-h-#{@size} max-w-#{@size} max-h-#{@size}"
+      size = ("%g" % @size) + @unit
+      "width: #{size}; height: #{size}; min-width: #{size};
+      min-height: #{size}; max-width: #{size}; max-height: #{size};"
     end
   end
 end
--- spec/components/tracks/playable_cover_photo_spec.rb
+++ spec/components/tracks/playable_cover_photo_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Tracks::PlayableCoverPhoto, type: :component do
     rendered = render_inline(described_class.new(track:, size: 6))
 
     expect(rendered).to have_css("img.cover-photo", count: 1)
-    expect(rendered).to have_css("div.w-6.h-6.aspect-square", count: 1)
+    expect(rendered).to have_css("[style*='width: 6rem;'][style*='height: 6rem;']", count: 1)
   end
 
   it "renders an icon if cover photo is not attached" do
@@ -17,12 +17,19 @@ RSpec.describe Tracks::PlayableCoverPhoto, type: :component do
     rendered = render_inline(described_class.new(track:, size: 8))
 
     expect(rendered).to have_css("svg.cover-photo", count: 1)
-    expect(rendered).to have_css("div.w-8.h-8.aspect-square", count: 1)
+    expect(rendered).to have_css("[style*='width: 8rem;'][style*='height: 8rem;']", count: 1)
   end
 
-  it "renders a play button" do
+  it "allows dynamic unit" do
+    rendered = render_inline(described_class.new(track:, size: 6, unit: "px"))
+
+    expect(rendered).to have_css("img.cover-photo", count: 1)
+    expect(rendered).to have_css("[style*='width: 6px;'][style*='height: 6px;']", count: 1)
+  end
+
+  it "renders play buttons" do
     rendered = render_inline(described_class.new(track:, size: 8))
 
-    expect(rendered).to have_css("svg.play-btn", count: 1)
+    expect(rendered).to have_css("svg", count: 2)
   end
 end
