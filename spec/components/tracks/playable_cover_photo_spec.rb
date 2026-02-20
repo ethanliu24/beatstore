30d77b2fe0329f42430fe82ca250f4e9b374c68c
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2026-02-20 00:27:20 -0500

    Add playable cover photo tracks to audio queue (#441)
    * set up component
    
    * add playabel cov photo tracks to queue
    
    * add queue scope to all cover photo instances
    
    * pass size as params in recommendations
    
    * fix tests

--- spec/components/tracks/playable_cover_photo_spec.rb
+++ spec/components/tracks/playable_cover_photo_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Tracks::PlayableCoverPhoto, type: :component do
   let(:track) { create(:track) }
 
   it "renders the cover photo if one is attached" do
-    rendered = render_inline(described_class.new(track:, size: 6))
+    rendered = render_inline(described_class.new(track:, queue_scope: "spec", size: 6))
 
     expect(rendered).to have_css("img.cover-photo", count: 1)
     expect(rendered).to have_css("[style*='width: 6rem;'][style*='height: 6rem;']", count: 1)
@@ -14,21 +14,21 @@ RSpec.describe Tracks::PlayableCoverPhoto, type: :component do
 
   it "renders an icon if cover photo is not attached" do
     track.cover_photo.purge
-    rendered = render_inline(described_class.new(track:, size: 8))
+    rendered = render_inline(described_class.new(track:, queue_scope: "spec", size: 8))
 
     expect(rendered).to have_css("svg.cover-photo", count: 1)
     expect(rendered).to have_css("[style*='width: 8rem;'][style*='height: 8rem;']", count: 1)
   end
 
   it "allows dynamic unit" do
-    rendered = render_inline(described_class.new(track:, size: 6, unit: "px"))
+    rendered = render_inline(described_class.new(track:, queue_scope: "spec", size: 6, unit: "px"))
 
     expect(rendered).to have_css("img.cover-photo", count: 1)
     expect(rendered).to have_css("[style*='width: 6px;'][style*='height: 6px;']", count: 1)
   end
 
   it "renders play buttons" do
-    rendered = render_inline(described_class.new(track:, size: 8))
+    rendered = render_inline(described_class.new(track:, queue_scope: "spec", size: 8))
 
     expect(rendered).to have_css("svg", count: 2)
   end
