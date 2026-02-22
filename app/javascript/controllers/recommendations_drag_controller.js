8ecf9d0a50c3888eee7019b2552fb67bb9407e41
ethanliu24 <liu.ethan2005@gmail.com>
2026-02-22 02:37:39 -0500

    use child cntrler

--- app/javascript/controllers/recommendations_drag_controller.js
+++ app/javascript/controllers/recommendations_drag_controller.js
@@ -1,16 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import DragAndSortController from "controllers/drag_and_sort_controller";
 
 // Connects to data-controller="recommendations-drag"
 export default class extends DragAndSortController {
-  connect() {
-    super();
-  }
-
   constructOptions() {
     var options = {
       handle: ".recs-drag-handle",
       animation: 500,
-      easing: "cubic-beizer(0.42, 0, 1, 1)"
+      easing: "cubic-beizer(0.42, 0, 1, 1)",
+      dataIdAttr: "data-recommendation-id",
+      onUpdate: () => {
+        console.log(this.sortable.toArray());
+      },
     };
 
     return options;
