7f7b341a0dd8fea87f50c0f5883124214c446fe9
ethanliu24 <liu.ethan2005@gmail.com>
2026-04-20 23:21:33 -0400

    add overview

--- app/helpers/license_helper.rb
+++ app/helpers/license_helper.rb
@@ -29,5 +29,64 @@ module LicenseHelper
   end
 
   def get_license_overview(license)
+    overview = []
+    contract = license.contract
+
+    if contract[:delivers_mp3]
+      tagged = license.contract_type == License.contract_types[:free]
+      overview << "1 MP3 File (#{tagged ? "Tagged" : "Untagged"})"
+    end
+
+    if contract[:delivers_wav]
+      overview << "1 WAV File (Untagged)"
+    end
+
+    if contract[:delivers_stems]
+      overview << "Trackout/Stems"
+    end
+
+    if contract.key?(:streams_allowed)
+      if contract[:streams_allowed].nil?
+        overview << "Unlimited Streams Cap"
+      elsif contract[:streams_allowed] > 0
+        overview << "#{contract[:streams_allowed]} Streams Cap"
+      end
+    end
+
+    if contract.key?(:distribution_copies)
+      if contract[:distribution_copies].nil?
+        overview << "Unlimited Units Cap"
+      elsif contract[:distribution_copies] > 0
+        overview << "#{contract[:distribution_copies]} Units Cap"
+      end
+    end
+
+    if contract.key?(:streams_allowed)
+      if contract[:streams_allowed].nil?
+        overview << "Unlimited Monetized MVs Cap"
+      elsif contract[:streams_allowed] > 0
+        overview << "#{contract[:streams_allowed]} Monetized MVs Cap"
+      end
+    end
+
+    if contract.key?(:monetized_videos)
+      if contract[:monetized_videos].nil?
+        overview << "Unlimited Monetized Music Videos"
+      elsif contract[:monetized_videos] > 0
+        overview << "#{contract[:monetized_videos]} Monetized MV Cap"
+      end
+    end
+
+    if contract.key?(:radio_stations_allowed)
+      if contract[:radio_stations_allowed].nil?
+        overview << "Unlimited Radio Stations Cap"
+      elsif contract[:radio_stations_allowed] > 0
+        overview << "#{contract[:radio_stations_allowed]} Radio Stations Cap"
+      end
+    end
+
+    overview << "Must credit in title (Prod. prodethan)"
+
+    overview
   end
 end
