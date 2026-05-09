43a8fdcde5c77e22cf4837ce1d0ee7cc880644b0
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-09 16:45:58 -0400

    add class to model versioning

--- app/lib/templates/legal_templates.rb
+++ app/lib/templates/legal_templates.rb
@@ -2,6 +2,16 @@
 
 module Templates
   class LegalTemplates < Templates
+    class Versioning
+      attr_accessor :tos, :privacy, :cookies
+
+      def initialize(tos:, pivacy:, cookies:)
+        @tos = tos
+        @privacy = privacy
+        @cookies = cookies
+      end
+    end
+
     VERSIONS = {
       tos: "2026.05.09.v1",
       privacy: "2026.04.28.v1",
@@ -9,6 +19,14 @@ module Templates
     }.freeze
 
     class << self
+      def current_versions
+        ::Versioning.new(
+          tos: VERSIONS[:tos],
+          privacy: VERSIONS[:privacy],
+          cookies: VERSIONS[:cookies],
+        )
+      end
+
       def read_tos
         read_template("templates/legal/terms_of_service.md")
       end
