92b7b2ed130dd4615c5359b7207a251ba616e47c
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-10 13:25:25 -0400

    add validation on model that accepted time cannot be in the past

--- app/models/users/legal_policies_acceptance.rb
+++ app/models/users/legal_policies_acceptance.rb
@@ -7,5 +7,25 @@ module Users
     belongs_to :user, optional: false
 
     validates :user_id, uniqueness: true
+    validate :timestamps_cannot_be_in_the_past, on: :update
+
+    private
+
+    def timestamps_cannot_be_in_the_past
+      timestamp_cols = [ :tos_accepted_at, :privacy_accepted_at, :cookies_accepted_at ]
+
+      timestamp_cols.each do |col|
+        next unless attribute_changed?(col)
+
+        previous_value = attribute_in_database(col)
+        current_value = send(col)
+
+        next if previous_value.nil? || current_value.nil?
+
+        if current_value < previous_value
+          errors.add(col, "cannot be updated to a time earlier than the previous acceptance")
+        end
+      end
+    end
   end
 end
