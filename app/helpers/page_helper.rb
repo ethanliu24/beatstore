4e171e58fce834b14b3bcdcdc70d88cf11f54050
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-10 20:32:07 -0400

    dont render popup on some pages

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -14,4 +14,11 @@ module PageHelper
   def hero_banner
     "stars.avif"
   end
+
+  def render_legal_policies_acceptance_popup?
+    current_page == "layouts/auth_main" ||
+    current_page?(terms_of_service_path) ||
+    current_page?(privacy_path) ||
+    current_page?(cookies_path)
+  end
 end
