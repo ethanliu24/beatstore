c5f74514f026d979382c83e48f6c7165ce88912e
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2026-05-18 15:18:40 -0400

    Rate limit application (#539)
    * add gem
    
    * basic config
    
    * config email throttle
    
    * rate limit free downloads
    
    * rate limit paid downloads
    
    * enable local dev caching
    
    * match url paths using str
    
    * add auth throttling test
    
    * add downloads throttling
    
    * actual order in tests
    
    * attempt to fix test
    
    * tf
    
    * fix test

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,135 @@
+class Rack::Attack
+  ### Configure Cache ###
+
+  # If you don't want to use Rails.cache (Rack::Attack's default), then
+  # configure it here.
+  #
+  # Note: The store is only used for throttling (not blocklisting and
+  # safelisting). It must implement .increment and .write like
+  # ActiveSupport::Cache::Store
+
+  # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  ### Throttle Spammy Clients ###
+
+  # If any single client IP is making tons of requests, then they're
+  # probably malicious or a poorly-configured scraper. Either way, they
+  # don't deserve to hog all of the app server's CPU. Cut them off!
+  #
+  # Note: If you're serving assets through rack, those requests may be
+  # counted by rack-attack and this throttle may be activated too
+  # quickly. If so, enable the condition to exclude them from tracking.
+
+  # Throttle all requests by IP (60rpm)
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
+  throttle("req/ip", limit: 300, period: 5.minutes) do |req|
+    req.ip # unless req.path.start_with?('/assets')
+  end
+
+  ### Prevent Brute-Force Login Attacks ###
+
+  # The most common brute-force login attack is a brute-force password
+  # attack where an attacker simply tries a large number of emails and
+  # passwords to see if any credentials match.
+  #
+  # Another common method of attack is to use a swarm of computers with
+  # different IPs to try brute-forcing a password for a specific account.
+
+  # Throttle POST requests to /login by IP address
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:auth/ip:#{req.ip}"
+  throttle("auth/ip", limit: 5, period: 20.seconds) do |req|
+    if auth_req?(req)
+      req.ip
+    end
+  end
+
+  # Throttle POST requests to /login by email param
+  #
+  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/email:#{normalized_email}"
+  #
+  # Note: This creates a problem where a malicious user could intentionally
+  # throttle logins for another user and force their login requests to be
+  # denied, but that's not very common and shouldn't happen to you. (Knock
+  # on wood!)
+  throttle("auth/email", limit: 5, period: 20.seconds) do |req|
+    if auth_req?(req)
+      # Normalize the email, using the same logic as your authentication process, to
+      # protect against rate limit bypasses. Return the normalized email if present, nil otherwise.
+      req.params["user"]["email"].to_s.downcase.gsub(/\s+/, "").presence
+    end
+  end
+
+  # Throttle free downloads
+  throttle("downloads/free/ip/burst",
+    limit: 10,
+    period: 10.seconds
+  ) do |req|
+    req.ip if free_download_req?(req)
+  end
+
+  throttle("downloads/free/ip/hourly",
+    limit: 500,
+    period: 1.hour
+  ) do |req|
+    req.ip if free_download_req?(req)
+  end
+
+  blocklist("fail2ban abusive free downloaders") do |req|
+    Rack::Attack::Fail2Ban.filter(
+      "download-abuse-#{req.ip}",
+      maxretry: 30,
+      findtime: 10.minutes,
+      bantime: 2.hours
+    ) do
+      req.env["rack.attack.matched"] == "downloads/free/ip/burst"
+    end
+  end
+
+  # Throttle paid downloads
+  throttle("downloads/paid/ip/burst",
+    limit: 15,
+    period: 10.seconds
+  ) do |req|
+    req.ip if paid_download_req?(req)
+  end
+
+  throttle("downloads/paid/ip/hourly",
+    limit: 100,
+    period: 10.minutes
+  ) do |req|
+    req.ip if paid_download_req?(req)
+  end
+
+  ### Custom Throttle Response ###
+
+  # By default, Rack::Attack returns an HTTP 429 for throttled responses,
+  # which is just fine.
+  #
+  # If you want to return 503 so that the attacker might be fooled into
+  # believing that they've successfully broken your app (or you just want to
+  # customize the response), then uncomment these lines.
+  # self.throttled_responder = lambda do |env|
+  #  [ 503,  # status
+  #    {},   # headers
+  #    ['']] # body
+  # end
+
+  private
+
+  def self.auth_req?(req)
+    [
+      req.path == "/" && req.post?,
+      req.path == "/sign_in" && req.post?
+    ].any?
+  end
+
+  def self.free_download_req?(req)
+    req.get? && req.path.match?(%r{\A/download/free_download/\d+(?:\.\w+)?\z})
+  end
+
+  def self.paid_download_req?(req)
+    req.get? && req.path.match?(%r{\A/download/order/\d+/item/\d+/files/\d+\z})
+  end
+end
