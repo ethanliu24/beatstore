2d508a292ffa754ca4a3b102070fbbbc2b607ae1
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-23 13:59:26 -0400

    use filtered method

--- app/controllers/users/hearts_controller.rb
+++ app/controllers/users/hearts_controller.rb
@@ -2,7 +2,7 @@ class Users::HeartsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    base_scope = current_user.hearted_tracks
+    base_scope = current_user.available_hearted_tracks
     @q = base_scope.ransack(params[:q], auth_object: current_user)
     queried_tracks = @q.result(distinct: true).includes(:tags)
     @pagy, @liked_tracks = pagy(queried_tracks, limit: 20)
