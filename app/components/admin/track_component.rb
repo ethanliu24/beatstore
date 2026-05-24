ac7bdbad99eb49b83e39c3956883ee45b872cd8a
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-24 14:56:05 -0400

    wip dynamic location

--- app/components/admin/track_component.rb
+++ app/components/admin/track_component.rb
@@ -2,13 +2,14 @@
 
 module Admin
   class TrackComponent < ApplicationComponent
-    def initialize(track:)
+    def initialize(track:, options: {})
       @track = track
+      @dropdown_position = options[:dropdown_position]
     end
 
     private
 
-    attr_reader :track
+    attr_reader :track, :dropdown_position
 
     def icon_dimension
       "w-3 h-3 aspect-square"
