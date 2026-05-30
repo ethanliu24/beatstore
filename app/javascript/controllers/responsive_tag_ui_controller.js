e5375bc1bae515e94b1388a14973cf1a3beaf4f1
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-30 11:12:44 -0400

    dynamically render dropdowns

--- app/javascript/controllers/responsive_tag_ui_controller.js
+++ app/javascript/controllers/responsive_tag_ui_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="responsive-tag-ui"
 export default class extends Controller {
-  static targets = ["tagsContainer", "tag"];
+  static targets = ["tagsContainer", "tag", "dropdownTrigger", "dropdownTag"];
 
   connect() {
     this.render();
@@ -16,19 +16,33 @@ export default class extends Controller {
     const conatinerLeft = container.left;
     const containerRight = container.right;
 
+    var overflow = 0;
+
     for (let i = 0; i < this.tagTargets.length; i++) {
       const tag = this.tagTargets[i];
+      const dropdownTag = this.dropdownTagTargets[i];
       const tagRect = tag.getBoundingClientRect();
 
-      if (!this.#inRange(conatinerLeft, containerRight, tagRect.left, tagRect.right)) {
-        tag.classList.add("hidden");
-      } else {
+      if (this.#inRange(conatinerLeft, containerRight, tagRect.left, tagRect.right)) {
         tag.classList.remove("hidden");
+        dropdownTag.classList.add("hidden");
+      } else {
+        tag.classList.add("hidden");
+        dropdownTag.classList.remove("hidden");
+        overflow++;
       }
+    }
+
+    if (overflow == 0) {
+      this.dropdownTriggerTarget.classList.add("hidden");
+    } else {
+      this.dropdownTriggerTarget.lastElementChild.innerText = `+${overflow}`;
+      this.dropdownTriggerTarget.classList.remove("hidden");
     }
 
     this.tagsContainerTarget.classList.add("justify-end");
     this.tagsContainerTarget.classList.remove("opacity-0");
+    this.dropdownTriggerTarget.classList.remove("opacity-0");
   }
 
   #inRange(boundLeft, boundRight, left, right) {
