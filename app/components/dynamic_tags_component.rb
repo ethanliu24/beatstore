e72c2a7f0ea3bd6b6d9d0dc19c5f0564926673f8
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-31 01:57:46 -0400

    move view to component

--- app/components/dynamic_tags_component.rb
+++ app/components/dynamic_tags_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class DynamicTagsComponent < ApplicationComponent
-  def initialize(tags:)
+  def initialize(tags:, resource:)
     @tags = tags
+    @resource = resource
   end
 
   private
 
-  attr_reader :tags
+  attr_reader :tags, :resource
 end
