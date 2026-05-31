5368150ad7a51a11522ed47572be25d4f6204235
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-31 02:07:25 -0400

    add rerender on resize config

--- app/components/dynamic_tags_component.rb
+++ app/components/dynamic_tags_component.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class DynamicTagsComponent < ApplicationComponent
-  def initialize(tags:, resource:, tag_data: {}, dropdown_tag_data: {})
+  def initialize(tags:, resource:, rerender_on_resize: true, tag_data: {}, dropdown_tag_data: {})
     @tags = tags
     @resource = resource
+    @rerender_on_resize = rerender_on_resize
     @tag_data = tag_data.merge({ responsive_tag_ui_target: "tag" })
     @dropdown_tag_data = dropdown_tag_data.merge({ responsive_tag_ui_target: "dropdownTag" })
   end
 
   private
 
-  attr_reader :tags, :resource, :tag_data, :dropdown_tag_data
+  attr_reader :tags, :resource, :rerender_on_resize, :tag_data, :dropdown_tag_data
 end
