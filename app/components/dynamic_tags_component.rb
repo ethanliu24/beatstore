40dbd38dfba06125ace562c4ba0de30e04ac1fe3
ethanliu24 <liu.ethan2005@gmail.com>
2026-05-31 02:02:49 -0400

    factor out some data attrs

--- app/components/dynamic_tags_component.rb
+++ app/components/dynamic_tags_component.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class DynamicTagsComponent < ApplicationComponent
-  def initialize(tags:, resource:)
+  def initialize(tags:, resource:, tag_data: {}, dropdown_tag_data: {})
     @tags = tags
     @resource = resource
+    @tag_data = tag_data.merge({ responsive_tag_ui_target: "tag" })
+    @dropdown_tag_data = dropdown_tag_data.merge({ responsive_tag_ui_target: "dropdownTag" })
   end
 
   private
 
-  attr_reader :tags, :resource
+  attr_reader :tags, :resource, :tag_data, :dropdown_tag_data
 end
