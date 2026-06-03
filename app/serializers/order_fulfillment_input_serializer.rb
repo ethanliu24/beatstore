71066b462f58107b55c14bf5911a17f3dbc5af32
ethanliu24 <liu.ethan2005@gmail.com>
2026-06-02 21:57:27 -0400

    add tests for serializer

--- app/serializers/order_fulfillment_input_serializer.rb
+++ app/serializers/order_fulfillment_input_serializer.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-class OrderFulfillmentSerializer < ActiveJob::Serializers::ObjectSerializer
+class OrderFulfillmentInputSerializer < ActiveJob::Serializers::ObjectSerializer
   def serialize(input)
     super(
       "order_id" => input.order.id,
       "customer_email" => input.customer_email,
-      "customer_name" => customer_name,
-      "amount_cents" => amount_cents,
-      "currency" => currency,
-      "stripe_charge_id" => stripe_charge_id,
+      "customer_name" => input.customer_name,
+      "amount_cents" => input.amount_cents,
+      "currency" => input.currency,
+      "stripe_charge_id" => input.stripe_charge_id,
     )
   end
 
   def deserialize(hash)
-    order = Order.find(h["order_id"])
-    FulfillOrderService::Input.nw(
+    order = Order.find(hash["order_id"])
+    FulfillOrderService::Input.new(
       order:,
       customer_email: hash["customer_email"],
       customer_name: hash["customer_name"],
