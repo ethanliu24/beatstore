46d0b7ab5463c43c4477c24a51dd9f4cdaec1766
ethanliu24 <liu.ethan2005@gmail.com>
2026-06-06 20:01:21 -0400

    replace in credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,6 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  # TODO replace with domain name
   config.action_mailer.default_url_options = { host: "prodethan.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
@@ -83,7 +82,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     "prodethan.com",     # Allow requests from example.com
-    /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+    /.*\.prodethan.com\.com/ # Allow requests from subdomains like `www.example.com`
   ]
 
   # Skip DNS rebinding protection for the default health check endpoint.
