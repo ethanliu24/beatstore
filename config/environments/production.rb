bcf3e25e62f29c069c39128cb9fadb147f57daf4
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2026-06-07 13:08:51 -0400

    Add stronger validation to config gem (#630)
    * fix test
    
    * touch on config initializer
    
    * default to using env and rails dig in config/
    
    * add env to ci
    
    * raise err on typo in Settings
    
    * update env setup doc
    
    * fix ci
    
    * hard require some env in config
    
    * typo
    
    * ci env
    
    * ci env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: Settings.host }
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST") }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -81,8 +81,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    Settings.host,     # Allow requests from example.com
-    /.*\.#{Regexp.escape(Settings.host)}\.com/ # Allow requests from subdomains like `www.example.com`
+    ENV.fetch("APP_HOST"),     # Allow requests from example.com
+    /.*\.#{ENV.fetch("APP_HOST")}\.com/ # Allow requests from subdomains like `www.example.com`
   ]
 
   # Skip DNS rebinding protection for the default health check endpoint.
