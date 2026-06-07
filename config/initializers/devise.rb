d999d48cc4971ac18575ac5b24fea11411db0e48
ethanliu24 <liu.ethan2005@gmail.com>
2026-06-06 20:15:42 -0400

    replace email in devise init

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "todo-update-actual-mailer@gmail.com"
+  config.mailer_sender = Rails.application.credentials.dig(:email, :domain, :prod)
 
   # Configure the class responsible to send e-mails.
   config.mailer = "Devise::Mailer"
