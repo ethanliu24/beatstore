238b49aa827ec495b44d0db602d8a9fc49801475
ethanliu24 <liu.ethan2005@gmail.com>
2026-06-07 19:21:03 -0400

    remove https protocol as its inferred from other configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,10 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = {
-    host: ENV.fetch("APP_HOST"),
-    protocol: :https
-  }
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST") }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   config.action_mailer.smtp_settings = {
