7926168b151d626b236e2e3d88d4fdf4f2ee2b7c
ethanliu24 <liu.ethan2005@gmail.com>
2026-06-20 12:28:41 -0400

    add metrics model and tracking method

--- app/models/metrics.rb
+++ app/models/metrics.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
-class InboundEmail < ApplicationRecord
-  validates :
+class Metrics < ApplicationRecord
+  validates :event_name, presence: true
+  validates :tags, presence: true
+  validates :prunes_at, presence: true
+  validates :created_at, presence: true
+
+  class << self
+    def track(event_name, tags: {}, prune: true, prunes_after: PruneTime::DEFAULT)
+      prunes_at = prune ? prunes_after : nil
+      created_at = Time.current
+
+      Metrics.create!(event_name:, tags:, prunes_at:, created_at:)
+    end
+  end
 end
