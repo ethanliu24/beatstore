7dcbfd59f0b9b8d52f27005e199251f19ca7779f
ethanliu24 <liu.ethan2005@gmail.com>
2026-06-21 12:29:15 -0400

    add metrics tab in sidebar

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 module AdminHelper
+  # controller => [pages]
+  SIDEBAR_NOT_RENDERED = {
+    "tracks" => [ "new", "edit" ],
+    "licenses" => [ "new", "edit" ],
+    "recommendations" => [ "new", "edit" ]
+  }
+
   def render_admin_sidebar?
-    (controller_name == "dashboards" && action_name == "show") ||
-    (controller_name == "tracks" && action_name == "index") ||
-    (controller_name == "licenses" && action_name == "index") ||
-    (controller_name == "transactions" && action_name == "index") ||
-    (controller_name == "transactions" && action_name == "show") ||
-    (controller_name == "recommendations" && action_name == "index")
+    !SIDEBAR_NOT_RENDERED.fetch(controller_name, []).include?(action_name)
   end
 end
