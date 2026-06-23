a2091bff37d08523e523c62e3c0d85d6a092f0e4
ethanliu24 <liu.ethan2005@gmail.com>
2026-06-22 23:24:48 -0400

    move chart metrics construction method in service

--- app/services/metrics/build_chart_metrics_service.rb
+++ app/services/metrics/build_chart_metrics_service.rb
@@ -6,5 +6,25 @@ module Metrics
       @event_name = event_name
       @window = window
     end
+
+    def line_chart_metrics(tags: {}, &tag_filter)
+      relation = Metric
+        .where(event_name: @event_name)
+        .where(created_at: BuildTimeFrameWindowService.time_frame(@window)..Time.current)
+        .where("tags @> ?", tags.to_json)
+
+      if tag_filter
+        ids = relation.select { |record| tag_filter.call(record.tags) }.map(&:id)
+        relation = Metric.where(id: ids)
+      end
+
+      metrics = BuildTimeFrameWindowService.group_metrics_by_time(
+        relation,
+        window: @window,
+        column: :created_at
+      )
+
+      metrics.count
+    end
   end
 end
