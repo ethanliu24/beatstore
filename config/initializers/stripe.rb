113554e83122d83d5a0605db0487b8bae48d45ce
Ethan Liu <94804114+ethanliu24@users.noreply.github.com>
2026-06-27 01:20:50 -0400

    use env var for secret key in dev mode (#691)

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,7 @@
 require "stripe"
 
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+Stripe.api_key = if Rails.env.production?
+  Rails.application.credentials.dig(:stripe, :secret_key)
+else
+  ENV["STRIPE_SECRET_KEY"]
+end
