557d7fe3e1c472353a63c0439dda5513dc23abe2
ethanliu24 <liu.ethan2005@gmail.com>
2026-07-11 23:02:24 -0400

    refactor and fully implement service

--- app/services/back_ups/upload_to_cloud_service.rb
+++ app/services/back_ups/upload_to_cloud_service.rb
@@ -15,43 +15,50 @@ module BackUps
       errors: Settings.backup.google_drive.folders.errors
     }.freeze
 
-    class << self
-      def call(backup, db_key:)
-        if backup.class.name != "BackUps::DumpDatabaseService::Result"
-          raise ArgumentError, "<backup> must be a BackUps::DumpDatabaseService::Result, but got #{backup.class.name}"
-        end
+    def initialize
+      @google_drive = Google::Apis::DriveV3::DriveService.new
+      configure_cloud
+    end
 
-        unless backup.ok?
-          raise ArgumentError, "<backup> data dump is not successful, cannot upload"
-        end
-
-        unless FOLDER_IDS.keys.include?(db_key.to_sym)
-          raise ArgumentError, "<db_key> #{db_key} is not a valid or defined database key, check config/database.yml"
-        end
-
-        drive_service = Google::Apis::DriveV3::DriveService.new
-        credentials = Google::Auth::UserRefreshCredentials.new(
-          client_id: GD_CLIENT_ID,
-          client_secret: GD_CLIENT_SECRET,
-          refresh_token: GG_REFRESH_TOKEN,
-          scope: API_SCOPE
-        )
-        drive_service.authorization = credentials
-
-        file_metadata = Google::Apis::DriveV3::File.new(
-          name: FILE_PATH.split("/").last,
-          parents: [ FOLDER ]
-        )
-
-        file = drive_service.create_file(
-          file_metadata,
-          fields: "id",
-          upload_source: FILE_PATH,
-          content_type: "application/octet-stream"
-        )
-
-        file
+    def call(backup, db_key:)
+      if backup.class.name != "BackUps::DumpDatabaseService::Result"
+        raise ArgumentError, "<backup> must be a BackUps::DumpDatabaseService::Result, but got #{backup.class.name}"
       end
+
+      unless backup.ok?
+        raise ArgumentError, "<backup> data dump is not successful, cannot upload"
+      end
+
+      unless FOLDER_IDS.keys.include?(db_key.to_sym)
+        raise ArgumentError, "<db_key> #{db_key} is not a valid or defined database key, check config/database.yml"
+      end
+
+      file_metadata = Google::Apis::DriveV3::File.new(
+        name: backup.filename,
+        parents: [ FOLDER_IDS[db_key] ]
+      )
+
+      file = @google_drive.create_file(
+        file_metadata,
+        fields: "id",
+        upload_source: backup.path,
+        content_type: backup.content_type
+      )
+
+      file
+    end
+
+    private
+
+    def configure_cloud
+      credentials = Google::Auth::UserRefreshCredentials.new(
+        client_id: Settings.google.api.oauth.client_id,
+        client_secret: Settings.google.api.oauth.client_secret,
+        refresh_token: Settings.google.api.oauth.google_drive_refresh_token,
+        scope: API_SCOPE
+      )
+
+      @google_drive.authorization = credentials
     end
   end
 end
